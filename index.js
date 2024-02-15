1c287ad7ffdbe511216d3b5811ad06d92cf93920
Mikayuko <146404280+Mikayuko@users.noreply.github.com>
2024-02-15 16:15:30 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const Ghibil_Studio = [{
+    "Image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/npOnzAbLh6VOIu3naU5QaEcTepo.jpg",
+    "Title": "Castle in the Sky",
+    "description": "The orphan Sheeta inherited a mysterious crystal that links her to the mythical sky-kingdom of Laputa. With the help of resourceful Pazu and a rollicking band of sky pirates, she makes her way to the ruins of the once-great civilization. Sheeta and Pazu must outwit the evil Muska, who plans to use Laputa's science to make himself ruler of the world.",
+    "Release Date": "1986"
+},
+{
+    "Image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg",
+        "Title": "Grave of the Fireflies",
+        "description": "In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.",
+        "Release Date": "1988"
+},
+{
+    "Image": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/rtGDOeG9LzoerkDGZF9dnVeLppL.jpg",
+        "Title": "My Neighbor Totoro",
+        "description": "https://image.tmdb.org/t/p/original/etqr6fOOCXQOgwrQXaKwenTSuzx.jpg",
+        "description": "Two sisters move to the country with their father in order to be closer to their hospitalized mother, and discover the surrounding trees are inhabited by Totoros, magical spirits of the forest. When the youngest runs away from home, the older sister seeks help from the spirits to find her.",
+        "Release Date": "1988"
+}
+]
+
+const jsonString = JSON.stringify(Ghibil_Studio);
+
+console.log(Ghibil_Studio);
